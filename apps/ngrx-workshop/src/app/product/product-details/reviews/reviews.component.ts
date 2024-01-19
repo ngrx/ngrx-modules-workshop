9f1290a1d8025a760ac2b4625f5213bd60bb5409
Alex Okrushko <alex@ts.dev>
2024-01-19 15:20:59 -0500

    m14: local state for reviews in signalStore

--- apps/ngrx-workshop/src/app/product/product-details/reviews/reviews.component.ts
+++ apps/ngrx-workshop/src/app/product/product-details/reviews/reviews.component.ts
@@ -1,43 +1,20 @@
-import { Component, Input, NgModule, OnInit } from '@angular/core';
+import { Component, NgModule, inject } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { RatingService } from '../../rating.service';
-import { BehaviorSubject, switchMap } from 'rxjs';
 import { MatCardModule } from '@angular/material/card';
 import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatInputModule } from '@angular/material/input';
 import { FormsModule } from '@angular/forms';
 import { MatButtonModule } from '@angular/material/button';
+import { reviewsStore } from './reviews.store';
 
 @Component({
   selector: 'ngrx-workshop-reviews',
   templateUrl: './reviews.component.html',
   styleUrl: './reviews.component.css',
+  providers: [reviewsStore],
 })
-export class ReviewsComponent implements OnInit {
-  @Input() productId!: string;
-
-  readonly reviewsRefresh$ = new BehaviorSubject<void>(undefined);
-
-  readonly reviews$ = this.reviewsRefresh$.pipe(
-    switchMap(() => this.ratingService.getReviews(this.productId))
-  );
-
-  constructor(private readonly ratingService: RatingService) {}
-
-  ngOnInit() {
-    this.reviewsRefresh$.next();
-  }
-
-  submit(review: { reviewer: string; reviewText: string }) {
-    this.ratingService
-      .postReview({
-        productId: this.productId,
-        ...review,
-      })
-      .subscribe(() => {
-        this.reviewsRefresh$.next();
-      });
-  }
+export class ReviewsComponent {
+  readonly reviewsStore = inject(reviewsStore);
 }
 
 @NgModule({
